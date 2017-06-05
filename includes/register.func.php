757ce3b45fb0b51ac3504dba14fe3a40d2439088
xxBei <zzw_bei@163.com>
2017-06-05 16:02:33 +0800

    新增密码验证、密码问题验证和密码答案验证

--- includes/register.func.php
+++ includes/register.func.php
@@ -49,4 +49,78 @@ function _check_username($_string,$min_num,$max_num){
 //     return mysqli_real_escape_string($_string);
     return $_string;
 }
+
+/**
+ * _check_password 验证密码
+ * @access public
+ * @param String $_first 密码
+ * @param String $_end 确认密码
+ * @param int $min_num 最小位数
+ * @return String $_first 加密的密码
+ */
+function _check_password($_first,$_end,$min_num){
+    //密码不能小于6位
+    if(strlen($_first) < 6){
+        exit(_alert_back('密码不能小于'.$min_num.'位'));
+    }
+    //验证两次密码是否一致
+    if($_first != $_end){
+        exit(_alert_back('两次密码不一致'));
+    }
+    //返回加密密码
+    return sha1($_first);
+}
+
+/**
+ * _check_question 验证问题
+ * @access public
+ * @param String $_question 问题
+ * @param int $min_num 最小位数
+ * @param int $max_num 最大位数
+ * @return string $_question 转义密码问题
+ */
+function _check_question($_question,$min_num,$max_num){
+    //密码问题不能小于4位或大于20位
+    if(mb_strlen($_question,'utf-8')<$min_num || mb_strlen($_question,'utf-8')>$max_num){
+        _alert_back('密码问题不能小于'.$min_num.'位或大于'.$max_num.'位');
+    }
+    //返回转义密码问题
+    return addslashes($_question);
+}
+
+/**
+ * _check_answer 验证密码答案
+ * @access public
+ * @param String $_ques 密码问题
+ * @param String $_answ 密码答案
+ * @param int $min_num 最小位数
+ * @param int $max_num 最大位数
+ * @return string $_answ 加密密码答案
+ */
+function _check_answer($_ques,$_answ,$min_num,$max_num){
+    //密码答案不能小于2位或大于20位
+    if(mb_strlen($_answ,'utf-8')<$min_num || mb_strlen($_answ,'utf-8')>$max_num){
+        _alert_back('密码答案不能小于'.$min_num.'位或大于'.$max_num.'位');
+    }
+    //密码答案不能和密码回答一致
+    if($_ques == $_answ){
+        _alert_back('密码答案不能和密码问题一致！');
+    }
+    //返回加密答案
+    return sha1($_answ);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
--- register.php
+++ register.php
@@ -19,13 +19,16 @@ if(@$_GET['action']=='register'){
     $yzm = strtolower($_POST['yzm']);
     //为防止恶意注册，跨站攻击
     if(!($yzm==$_SESSION['code'])){
-        _alert_back('验证码错误！');
+        exit(_alert_back('验证码错误！'));
     }
     //引入校验文件
     include ROOT_PATH.'includes/register.func.php';
     //定义一个数组来存放表单数据
     $clean = array();
     $clean['username'] = _check_username($_POST['username'],2,20);
+    $clean['password'] = _check_password($_POST['password'], $_POST['notpassword'], 6);
+    $clean['question'] = _check_question($_POST['question'], 2, 20);
+    $clean['answer'] = _check_answer($_POST['question'], $_POST['answer'], 2, 20);
     print_r($clean);
 
 
@@ -48,9 +51,9 @@ if(@$_GET['action']=='register'){
 				<dt>请仔细填写一下信息</dt>
 				<dd>用户名称：<input type="text" name="username" class="text"/>　<span>(*必填，最少为2位)</span></dd>
 				<dd>密　　码：<input type="password" name="password" class="text"/>　(*必填，最少为6位)</dd>
-				<dd>确认密码：<input type="password" name="password" class="text"/>　(*必填，最少为6位)</dd>
-				<dd>密码提示：<input type="text" name="passt" class="text"/>　(*必填，最少为2位)</dd>
-				<dd>密码回答：<input type="text" name="passd" class="text"/>　(*必填，最少为2位)</dd>
+				<dd>确认密码：<input type="password" name="notpassword" class="text"/>　(*必填，最少为6位)</dd>
+				<dd>密码提示：<input type="text" name="question" class="text"/>　(*必填，最少为2位)</dd>
+				<dd>密码回答：<input type="text" name="answer" class="text"/>　(*必填，最少为2位)</dd>
 				<dd>性　　别：<input type="radio" name='sex' value='男' checked="checked"/>男　<input type="radio" name='sex' value='女'/>女</dd>
 				<dd><input type="hidden" name ="faces" value="face/1.png"><img src="face/1.png" class='face' id="userimg"/></dd>
 				<dd>电子邮件：<input type="text" name='email' class='text'/></dd>
