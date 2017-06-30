ee2f28b85b07932e7d4d20d9000eb320ad4a7449
xxBei <zzw_bei@163.com>
2017-06-30 16:56:49 +0800

    实现添加好友功能，并增加了一些判断，重复验证问题和不是被请求方不能操作验证

--- member_friend.php
+++ member_friend.php
@@ -14,23 +14,42 @@ require dirname(__FILE__).'/includes/common.inc.php';
 if(!isset($_COOKIE['username'])){
     _alert_back('请先登录');
 }
-if(@$_GET['action'] == 'add' && isset($_POST['ids'])){
-    $_clean = array();
-    $_clean['id'] = _mysql_string(implode(',',$_POST['ids']));
-    //危险操作，防止cookie伪造，验证唯一标识符
-    $_rows = _mysql_fetch_array("SELECT 
-                                              gb_uniqid 
-                                       FROM 
-                                              gb_friend 
-                                       WHERE 
-                                              gb_username = '{$_COOKIE['username']}' 
-                                       LIMIT 
-                                              1
-                                ");
+if(@$_GET['action']=='add' && isset($_GET['id'])){
+    $_uniqid_row = _mysql_fetch_array("SELECT gb_uniqid FROM gb_manager WHERE gb_username = '{$_COOKIE['username']}'");
+    if(!!$_uniqid_row){
+        _uniqid($_COOKIE['uniqid'],$_uniqid_row['gb_uniqid']);
+        //验证好友
+        $_result1 = _mysql_fetch_array("SELECT gb_state FROM gb_friend WHERE gb_id = '{$_GET['id']}'");
+        if($_result1['gb_state'] == '1'){
+            _alert_back('你们已经是好友了');
+        }
+        _mysql_query("UPDATE gb_friend SET gb_state = '1' WHERE gb_id = '{$_GET['id']}' AND gb_touser = '{$_COOKIE['username']}'");
+        if(_mysql_affected_rows() == 1){
+            _mysql_close();
+            _location('验证成功','member_friend.php');
+        }else{
+            _mysql_close();
+            _alert_back('验证失败');
+        }
+        $_result2 = _mysql_fetch_array("SELECT gb_touser FROM gb_friend WHERE gb_id = '{$_GET['id']}'");
+        if($_result2['gb_touser'] != $_COOKIE['username']){
+            _location('你没有权限','member_friend.php');
+        }
+    }else{
+        _alert_back('非法登录');
+    }
 }
 //调用分页函数
 global $_pagesize,$_pagenum;
-_page_main("SELECT gb_id FROM gb_friend WHERE gb_touser='{$_COOKIE['username']}'",15);
+_page_main("SELECT 
+                          gb_id 
+                   FROM 
+                          gb_friend 
+                   WHERE 
+                          gb_touser='{$_COOKIE['username']}' 
+                   OR 
+                          gb_fromuser='{$_COOKIE['username']}'",
+            15);
 //显示好友列表
 if($_pagenum < 0){
     $_pagenum = 1;
@@ -45,6 +64,8 @@ if($_pagenum < 0){
                                       gb_friend
                                 WHERE 
                                       gb_touser = '{$_COOKIE['username']}'
+                                OR
+                                      gb_fromuser = '{$_COOKIE['username']}'
                              ORDER BY
                                       gb_date DESC
                                  LIMIT
@@ -69,7 +90,7 @@ if($_pagenum < 0){
         <h2>好友管理中心</h2>
         <form action="?action=add" method="post">
             <table cellspacing="1">
-                <tr><th>请求用户</th><th>请求内容</th><th>时间</th><th>状态</th><th>操作</th></tr>
+                <tr><th>好友</th><th>请求内容</th><th>时间</th><th>状态</th><th>操作</th></tr>
                 <?php
                     if(empty($_result)){
                         exit();
@@ -77,24 +98,35 @@ if($_pagenum < 0){
                     while (!!$_rows = _mysql_fetch_array_list($_result)){
                         $_html = array();
                         $_html['id'] = $_rows['gb_id'];
-                        $_html['fromuser'] = $_rows['gb_fromuser'];
+                        $_html['touser'] = $_rows['gb_touser']; //被添加的
+                        $_html['fromuser'] = $_rows['gb_fromuser'];//添加的
                         $_html['content'] = $_rows['gb_content'];
                         $_html['date'] = $_rows['gb_date'];
                         $_html = _htmlspecialchars($_html);
                         $_html['state'] = $_rows['gb_state'];
-                        if($_html['state'] == 0){
-                            $_html['state'] = '未添加';
-                        }else{
-                            $_html['state'] = '已添加';
+                        if($_html['touser'] == $_COOKIE['username']){
+                            $_html['friend'] = $_html['fromuser'];
+                            if(empty($_html['state'])){
+                                $_html['state'] = '<span style="color: #1195ff;">你未验证</span>';
+                            }else{
+                                $_html['state'] = '<span style="color: #0f0;">通过</span>';
+                            }
+                        }else if($_html['fromuser'] == $_COOKIE['username']){
+                            $_html['friend'] = $_html['touser'];
+                            if(empty($_html['state'])){
+                                $_html['state'] = '<span style="color:#f00">对方未验证</span>';
+                            }else{
+                                $_html['state'] = '<span style="color: #0f0;">通过</span>';
+                            }
                         }
                 ?>
-                <tr><td><?php echo $_html['fromuser']?></td><td title="<?php echo $_html['content']?>"><?php echo _title($_html['content'])?></td><td><?php echo $_html['date']?></td><td>
-                        <?php echo $_html['state']?></td><td><input name="ids[]" value="<?php echo $_html['id']?>" type="checkbox"/></td></tr>
+                <tr><td><?php echo $_html['friend']?></td><td title="<?php echo $_html['content']?>"><?php echo _title($_html['content'])?></td><td><?php echo $_html['date']?></td><td>
+                        <a href="?action=add&id=<?php echo $_html['id'];?>"><?php echo $_html['state']?></a></td><td><input name="ids[]" value="<?php echo $_html['id']?>" type="checkbox"/></td></tr>
                 <?php
                     }
                     _mysql_free_result($_result);
                 ?>
-                <tr><td colspan="5"><label for="all">全选<input type="checkbox" name="chkall" id="all"></label><input type="submit" id="delete" value="批量删除" name="delete"><input type="button" id="add" value="添加" name="add"></td></tr>
+                <tr><td colspan="5"><label for="all">全选<input type="checkbox" name="chkall" id="all"></label><input type="submit" id="delete" value="批量删除" name="delete"></td></tr>
             </table>
         </form>
         <?php
