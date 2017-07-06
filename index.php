f9d1da760b9333b5902ebc2a4a88de32a4927028
xxBei <zzw_bei@163.com>
2017-07-06 14:11:22 +0800

    完善帖子样式

--- index.php
+++ index.php
@@ -21,16 +21,16 @@ $_html = _get_xml('new.xml');
 		<h2>帖子列表</h2>
         <a href="post.php" class="post"> </a>
         <ul class="article">
-            <li class="icon1"><em>阅读数(<strong>72</strong>) 评论数(<strong>2</strong>)</em> <a href="#">创意时代：解密QQ仙侠传美术创意设计</a></li>
-            <li class="icon2"><em>阅读数(<strong>194</strong>) 评论数(<strong>7</strong>)</em> <a href="#">天掉下馅饼《游戏人生》变装拿大奖</a></li>
-            <li class="icon3"><em>阅读数(<strong>39</strong>) 评论数(<strong>0</strong>)</em> <a href="#">格斗大作《街头霸王4》PC版即将公布</a></li>
-            <li class="icon4"><em>阅读数(<strong>46</strong>) 评论数(<strong>0</strong>)</em> <a href="#">暗黑魔幻《炼狱》4月19正式开放封测</a></li>
-            <li class="icon5"><em>阅读数(<strong>23</strong>) 评论数(<strong>0</strong>)</em> <a href="#">永恒之塔的日子有一种自豪叫做牺牲</a></li>
-            <li class="icon6"><em>阅读数(<strong>33</strong>) 评论数(<strong>0</strong>)</em> <a href="#">盘点多年以后你还刻骨铭心的十款游戏</a></li>
-            <li class="icon7"><em>阅读数(<strong>23</strong>) 评论数(<strong>0</strong>)</em> <a href="#">炫舞吧 内测火爆 引领休闲舞蹈网游</a></li>
-            <li class="icon8"><em>阅读数(<strong>22</strong>) 评论数(<strong>0</strong>)</em> <a href="#">姚仙亲自主刀 《仙剑5》剧透曝光?</a></li>
-            <li class="icon9"><em>阅读数(<strong>12</strong>) 评论数(<strong>1</strong>)</em> <a href="#">新概念战车网游《钢铁围攻》24日封测</a></li>
-            <li class="icon10"><em>阅读数(<strong>251</strong>) 评论数(<strong>3</strong>)</em> <a href="#">完美国际新副本即将推出 背景揭秘</a></li>
+            <li class="icon1"><em><a href="">阅读数(<strong>72</strong>)</a> <a href="">评论数(<strong>2</strong>)</a></em> <a href="#">创意时代：解密QQ仙侠传美术创意设计</a></li>
+            <li class="icon2"><em><a href="">阅读数(<strong>194</strong>)</a> <a href="">评论数(<strong>7</strong>)</a></em> <a href="#">天掉下馅饼《游戏人生》变装拿大奖</a></li>
+            <li class="icon3"><em><a href="">阅读数(<strong>39</strong>)</a> <a href="">评论数(<strong>0</strong>)</a></em> <a href="#">格斗大作《街头霸王4》PC版即将公布</a></li>
+            <li class="icon4"><em><a href="">阅读数(<strong>46</strong>)</a> <a href="">评论数(<strong>0</strong>)</a></em> <a href="#">暗黑魔幻《炼狱》4月19正式开放封测</a></li>
+            <li class="icon5"><em><a href="">阅读数(<strong>23</strong>)</a> <a href="">评论数(<strong>0</strong>)</a></em> <a href="#">永恒之塔的日子有一种自豪叫做牺牲</a></li>
+            <li class="icon6"><em><a href="">阅读数(<strong>33</strong>)</a> <a href="">评论数(<strong>0</strong>)</a></em> <a href="#">盘点多年以后你还刻骨铭心的十款游戏</a></li>
+            <li class="icon7"><em><a href="">阅读数(<strong>23</strong>)</a> <a href="">评论数(<strong>0</strong>)</a></em> <a href="#">炫舞吧 内测火爆 引领休闲舞蹈网游</a></li>
+            <li class="icon8"><em><a href="">阅读数(<strong>22</strong></a> <a href="">评论数(<strong>0</strong>)</a></em> <a href="#">姚仙亲自主刀 《仙剑5》剧透曝光?</a></li>
+            <li class="icon9"><em><a href="">阅读数(<strong>12</strong>)</a> <a href="">评论数(<strong>1</strong>)</a></em> <a href="#">新概念战车网游《钢铁围攻》24日封测</a></li>
+            <li class="icon10"><em><a href="">阅读数(<strong>251</strong>)</a> <a href="">评论数(<strong>3</strong>)</a></em> <a href="#">完美国际新副本即将推出 背景揭秘</a></li>
         </ul>
 	</div>
 	<div id="user">
